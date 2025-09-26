1a1d08b133fbc7975da236810bee0276e08361a9
Ege Çelikçi <ege@celikci.me>
2025-09-26 23:51:28 +0300

    first commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,12 @@
+// Module Preload
+import "vite/modulepreload-polyfill";
+
+import "../styles/main.scss";
+
+import "./common/navigation";
+/*
+import './common/lazyload'
+import './common/infinitescroll'
+import './common/preload'
+import './common/register-serviceworker'
+*/
---
+++ src/data/site.js
@@ -0,0 +1,9 @@
+export default {
+  title: "egecelikci",
+  host: "ege.celikci.me",
+  description: "website",
+  lang: "en",
+  locale: "en_US",
+  url: "https://ege.celikci.me",
+  repo: "https://github.com/egecelikci/ege.celikci.me",
+};
