dfc7fe93aeb18445bfdc6990904a64d79cbe9c80
Ege Çelikçi <ege@celikci.me>
2025-10-02 21:34:41 +0300

    misc

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,12 +1,12 @@
 // Module Preload
 import "vite/modulepreload-polyfill";
 
+// Main Stylesheet
 import "../styles/main.scss";
 
+// Common Modules
 import "./common/navigation";
-/*
-import './common/lazyload'
-import './common/infinitescroll'
-import './common/preload'
-import './common/register-serviceworker'
-*/
+import "./common/lazyload";
+import "./common/preload";
+import "./common/speedlify";
+import "./common/register-serviceworker";
