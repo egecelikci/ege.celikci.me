eab0511488b68bd1d749af1414bfb6905bc59bae
Ege Çelikçi <ege@celikci.me>
2025-11-19 02:38:43 +0300

    improve navigation

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,7 +6,6 @@ import "../styles/main.scss";
 
 // Common Modules
 import "./common/lazyload";
-import "./common/navigation";
 import "./common/preload";
 import "./common/register-serviceworker";
 import "./common/speedlify";
