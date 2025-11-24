a09271544bec0f9ae93e4a6bfbc5f91053970b9d
Ege Çelikçi <ege@celikci.me>
2025-11-24 05:25:50 +0300

    various improvements

--- utils/markdown.js
+++ utils/markdown.js
@@ -1,23 +1,9 @@
 import markdownIt from "markdown-it";
 import anchor from "markdown-it-anchor";
 
-const anchorSlugify = (s) =>
-  encodeURIComponent(
-    "h-" +
-      String(s)
-        .trim()
-        .toLowerCase()
-        .replace(/[.,\/#!$%\^&\*;:{}=_`~()]/g, "")
-        .replace(/\s+/g, "-"),
-  );
-
 const anchorOpts = {
-  symbol: "#",
+  placement: "before",
   class: "heading-anchor",
-  visuallyHiddenClass: "sr-only",
-  style: "visually-hidden",
-  assistiveText: (title) => `Permalink to “${title}”`,
-  renderHref: anchorSlugify,
 };
 
 export default markdownIt({
@@ -25,5 +11,5 @@ export default markdownIt({
   breaks: true,
   typographer: true,
 }).use(anchor, {
-  permalink: anchor.permalink.linkAfterHeader(anchorOpts),
+  permalink: anchor.permalink.linkInsideHeader(anchorOpts),
 });
