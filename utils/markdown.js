d25096254f7a6c6f146fa9a8e0bd2bc0df9f80f1
Ege Çelikçi <ege@celikci.me>
2025-11-26 11:37:05 +0300

    misc

--- utils/markdown.js
+++ utils/markdown.js
@@ -1,6 +1,15 @@
 import markdownIt from "markdown-it";
 import anchor from "markdown-it-anchor";
 
+const anchorSlugify = (s) =>
+  encodeURIComponent(
+    String(s)
+      .trim()
+      .toLowerCase()
+      .replace(/[.,/#!$%^&*;:{}=_`~()]/g, "")
+      .replace(/\s+/g, "-"),
+  );
+
 const anchorOpts = {
   placement: "after",
   class: "heading-anchor",
@@ -12,4 +21,5 @@ export default markdownIt({
   typographer: true,
 }).use(anchor, {
   permalink: anchor.permalink.linkInsideHeader(anchorOpts),
+  slugify: anchorSlugify,
 });
