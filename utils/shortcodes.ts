1e2506adc679339ec7476f15169e8140a44dce11
Ege Çelikçi <ege@celikci.me>
2025-12-07 17:44:22 +0300

    hopefully faster builds

--- utils/shortcodes.ts
+++ utils/shortcodes.ts
@@ -1,5 +1,4 @@
 // deno-lint-ignore-file no-explicit-any
-import Image from "npm:@11ty/eleventy-img@^6.0.4";
 import { Callout, Icon, renderTags, } from "./components.ts";
 
 export const syncShortcodes: Record<string, any> = {
@@ -10,29 +9,4 @@ export const syncShortcodes: Record<string, any> = {
 export const pairedShortcodes: Record<string, any> = {
   callout: Callout,
 };
-
-export const ogImage = async (filepath: string,): Promise<string> => {
-  if (!filepath || filepath.startsWith("http",)) return filepath;
-
-  try {
-    const metadata = await Image(filepath, {
-      widths: [1200,],
-      formats: ["jpeg",],
-      urlPath: "/assets/images/og/",
-      outputDir: "./dist/assets/images/og/",
-      cacheOptions: {
-        duration: "1d",
-      },
-    },);
-
-    const data = metadata.jpeg[0];
-    return data.url;
-  } catch (e) {
-    console.error(`[ogImage] Error processing ${filepath}:`, e,);
-    return "";
-  }
-};
-
-export const asyncShortcodes: Record<string, any> = {
-  ogImage,
-};
+export const asyncShortcodes: Record<string, any> = {};
