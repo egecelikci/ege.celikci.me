2855a5ec2ea473834cb0818e1313fee6d9d79eeb
Ege Çelikçi <ege@celikci.me>
2025-12-07 14:16:51 +0300

    node.js → deno.ts

---
+++ utils/shortcodes.ts
@@ -0,0 +1,13 @@
+// deno-lint-ignore-file no-explicit-any
+import { Callout, Icon, renderTags, } from "./components.ts";
+
+export const syncShortcodes: Record<string, any> = {
+  icon: Icon,
+  renderTags: renderTags,
+};
+
+export const pairedShortcodes: Record<string, any> = {
+  callout: Callout,
+};
+
+export const asyncShortcodes: Record<string, any> = {};
