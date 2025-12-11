1239c4d3b9014681f9310f8dab9b5cf787a16f28
Ege Çelikçi <ege@celikci.me>
2025-12-11 23:31:58 +0300

    add _theme.scss

--- utils/shortcodes.ts
+++ utils/shortcodes.ts
@@ -1,10 +1,9 @@
 // deno-lint-ignore-file no-explicit-any
-import { Callout, Icon, renderTags, themeCSS, } from "./components.ts";
+import { Callout, Icon, renderTags, } from "./components.ts";
 
 export const syncShortcodes: Record<string, any> = {
   icon: Icon,
   renderTags: renderTags,
-  themeCSS: themeCSS,
 };
 
 export const pairedShortcodes: Record<string, any> = {
