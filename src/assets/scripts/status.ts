d8614756e4c84ca522b9ffd007f070cd03a1cefc
Ege Çelikçi <ege@celikci.me>
2025-12-20 15:01:19 +0300

    refactor dependencies to use inline npm imports

--- src/assets/scripts/status.ts
+++ src/assets/scripts/status.ts
@@ -1,5 +1,5 @@
-import { gsap, } from "gsap";
-import ScrambleTextPlugin from "gsap/ScrambleTextPlugin.js";
+import gsap from "npm:gsap@^3.14.1";
+import ScrambleTextPlugin from "npm:gsap@^3.14.1/ScrambleTextPlugin";
 
 gsap.registerPlugin(ScrambleTextPlugin,);
 
