73ee090c05c109fe63f59e0957ef479ef3501627
Ege Çelikçi <ege@celikci.me>
2025-12-21 20:04:29 +0300

    fix afterbuild script & font preloading

--- src/assets/scripts/common/register-serviceworker.ts
+++ src/assets/scripts/common/register-serviceworker.ts
@@ -4,9 +4,9 @@ declare const process: { env: { MODE: string; }; };
 if ("serviceWorker" in navigator) {
   // FIX 3: Allow it to run if it's production OR if you are testing locally
   // (You can remove the explicit 'development' check later)
-  if (process.env.MODE === "production" || process.env.MODE === "development") {
+  if (process.env.MODE === "production") {
     
-    window.addEventListener("load", () => {
+    globalThis.addEventListener("load", () => {
       navigator.serviceWorker.register("/sw.js")
         .then(registration => {
           console.log("SW registered: ", registration);
