0681f47bbe4e55d6fbfb584848e4d4857e7bddc6
Ege Çelikçi <ege@celikci.me>
2026-02-10 22:14:57 +0300

    update dependencies

--- utils/filters.ts
+++ utils/filters.ts
@@ -1,7 +1,7 @@
 import * as path from "jsr:@std/path";
-import { random, } from "npm:lodash-es@4.17.22";
+import { random, } from "npm:lodash-es@4.17.23";
 import { DateTime, } from "npm:luxon@^3.7.2";
-import sanitizeHTML from "npm:sanitize-html@2.12.1";
+import sanitizeHTML from "npm:sanitize-html@2.17.0";
 
 import siteData from "../src/_data/site.ts";
 
