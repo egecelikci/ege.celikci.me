ce17a46fa8ad64701ce1d47aa174aa542045b799
Ege Çelikçi <ege@celikci.me>
2026-05-07 00:33:14 +0300

    rm desc

--- _config/metadata.ts
+++ _config/metadata.ts
@@ -6,7 +6,6 @@
 export const site = {
   title: "ege.celikci.me",
   host: "ege.celikci.me",
-  description: "my personal website, blog and portfolio.",
   lang: "en",
   locale: "en_US",
   url: "https://ege.celikci.me",
