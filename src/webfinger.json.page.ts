1816b3eb51952537c3d17d11c7e9499866146d81
Ege Çelikçi <ege@celikci.me>
2026-06-30 17:02:54 +0300

    feat(auth): implement dynamic webfinger endpoint for codeberg oidc

---
+++ src/webfinger.json.page.ts
@@ -0,0 +1,18 @@
+import { author } from "../_config/metadata.ts";
+
+export const url = "/webfinger.json";
+export const layout = null;
+
+export default function () {
+  const data = {
+    subject: `acct:${author.email}`,
+    links: [
+      {
+        rel: "http://openid.net/specs/connect/1.0/issuer",
+        href: "https://codeberg.org/",
+      },
+    ],
+  };
+
+  return JSON.stringify(data, null, 2);
+}
