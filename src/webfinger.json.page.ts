ca5d4d377077e0e22f35427fcdd7c8476100ace2
Ege Çelikçi <ege@celikci.me>
2026-06-30 17:19:24 +0300

    fix(webfinger)

--- src/webfinger.json.page.ts
+++ src/webfinger.json.page.ts
@@ -9,7 +9,7 @@ export default function () {
     links: [
       {
         rel: "http://openid.net/specs/connect/1.0/issuer",
-        href: "https://codeberg.org/",
+        href: "https://codeberg.org",
       },
     ],
   };
