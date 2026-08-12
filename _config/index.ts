ee7657093a2521f9e0889428996d15a8aafa5746
Ege Çelikçi <ege@celikci.me>
2026-08-12 14:55:22 +0300

    fix(auth): point openid issuer at balcova identity

--- _config/index.ts
+++ _config/index.ts
@@ -148,7 +148,7 @@ export default function () {
           links: [
             {
               rel: "http://openid.net/specs/connect/1.0/issuer",
-              href: "https://codeberg.org",
+              href: "https://id.balcova.online",
             },
           ],
         },
