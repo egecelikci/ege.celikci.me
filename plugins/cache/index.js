1fcbbfd16549e74f113492229694b8a99a6c72ac
Ege Çelikçi <ege@celikci.me>
2026-08-13 15:08:32 +0300

    ci(netlify): re-add build cache plugin

---
+++ plugins/cache/index.js
@@ -0,0 +1,9 @@
+export default {
+  async onPreBuild({ utils }) {
+    await utils.cache.restore("./_cache");
+  },
+
+  async onPostBuild({ utils }) {
+    await utils.cache.save("./_cache");
+  },
+};
